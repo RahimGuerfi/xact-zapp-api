e0feb41728b0747ac345d63cda4f8f59dccd83de
RahimGuerfi <89612748+RahimGuerfi@users.noreply.github.com>
2021-11-22 00:22:19 +0100

    Updated

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'fastify-helmet';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import compression from 'fastify-compress';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppConfig, appConfiguration } from '@xact-checkout/api/configuration';
 
@@ -15,6 +17,23 @@ async function bootstrap() {
   );
 
   const appConfig = app.get<AppConfig>(appConfiguration.KEY);
+
+  await app.register(compression, { encodings: ['gzip', 'deflate'] });
+  await app.register(helmet.fastifyHelmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [`'self'`, `'unsafe-inline'`],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+      },
+    },
+  });
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  await app.register(require('fastify-rate-limit'), {
+    max: 100,
+    timeWindow: '1 minute',
+  });
 
   const globalPrefix = 'api';
 
